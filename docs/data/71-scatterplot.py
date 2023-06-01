25d24bc9043d7b3b349c80fedbefc66489ad2656
Ken Wakita <wakita@is.titech.ac.jp>
2023-06-01 13:11:15 +0900

    scatterplot

---
+++ docs/data/71-scatterplot.py
@@ -0,0 +1,12 @@
+scatterplot = dict(
+  name='Scatterplot',
+  what='Table: two quantitative value attributes',
+  how=dict(explanation='Express values with horizontal and vertical spatial position and point marks',
+           used=dict(mark='point',
+                     quantitative=['position on common scale (horizontal)',
+                                   'position on common scale (vertical)'],
+                     category=['point'])
+  why=dict(find=['trends', 'outliers', 'distribution', 'correlation'],
+           locate='cate clusters')
+  scale=dict(items='hundreds')
+)
